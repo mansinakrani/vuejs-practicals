f77b45d1769116a45b14f4539f9f0a05c2a6641e
mansinakrani <mansinakrani006@gmail.com>
2022-04-06 16:38:52 +0530

    docs:Practical-3

---
+++ practical-3/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+// global component navbar
+import { createApp } from "vue";
+import Navbar from "./components/NavBar.vue";
+const app = createApp({});
+app.component("NavBar", Navbar);
+</script>
+
+<template>
+  <Navbar heading="Car Showroom" />
+  <GalleryCard />
+</template>
+
+<script>
+import GalleryCard from './components/GalleryCard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    GalleryCard
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
