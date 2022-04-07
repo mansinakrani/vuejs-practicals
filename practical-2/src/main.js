7e5831c5a8ead90b6ec80ca4364bace87ff2f15d
mansinakrani <mansinakrani006@gmail.com>
2022-04-07 11:59:23 +0530

    change code based on mentor's feedback

--- practical-2/src/main.js
+++ practical-2/src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// import NavBar from "./components/NavBar.vue";
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+// createApp(App).component(Navbar).mount("#app");
+const app = createApp(App);
+
+// app.component("NavBar", NavBar); // global registration - can be used anywhere
+
+app.mount("#app");
