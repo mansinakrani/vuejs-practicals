e5d9392189fed3640f371a453e50af0e50da3969
mansinakrani <mansinakrani006@gmail.com>
2022-04-12 18:56:36 +0530

    change code based on mentor's feedback

--- practical-3/src/App.vue
+++ practical-3/src/App.vue
@@ -1,32 +1,19 @@
 <template>
   <NavBar heading="Car Showroom" />
-  <div class="row">
-    <div class="column">
-        <div v-for="carItem in carData" :key="carItem.id" class="dataContainer">
-          <GalleryCard 
-            :carImage="carItem.carImage"
-            :carName="carItem.carName"
-            :carDetails="carItem.carDetails"
-            :carPrice="carItem.carPrice"
-            :id="carItem.id"        
-            />
-        </div>
-    </div>
-  </div>
+  <HomeView />
 </template>
 
 <script>
-import GalleryCard from './components/GalleryCard.vue';
-import jsonData from "./components/jsonData.json";
+import HomeView from './components/HomeView.vue';
 
 export default {
   name: 'App',
   components: {
-    GalleryCard,
+    HomeView,
   },
   data() {
     return {
-      carData: jsonData,
+      
     };
   },
 }
@@ -40,31 +27,4 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
-
-.column {
-  display: flex;
-  flex-direction: row;
-  float: left;
-  justify-content: center
-}
-
-@media screen and (max-width: 650px) {
-  .column {
-    width: 100%;
-    display: block;
-    margin-bottom: 10px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center
-  }
-}
-
-.dataContainer{
- background-color: rgb(187, 234, 235);
- border-radius: 15px;
- margin: 25px;
- width: 400px;
- box-shadow: rgba(112, 112, 220, 0.1) 0px 8px 24px, rgba(223, 28, 28, 0.1) 0px 16px 56px, #f265651a 0px 24px 80px;
-}
-
 </style>
---
+++ practical-3/src/components/HomeView.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="row">
+    <div class="column">
+        <div v-for="carItem in carData" :key="carItem.id" class="dataContainer">
+          <GalleryCard 
+            :carImage="carItem.carImage"
+            :carName="carItem.carName"
+            :carDetails="carItem.carDetails"
+            :carPrice="carItem.carPrice"
+            :id="carItem.id"        
+            />
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import GalleryCard from './GalleryCard.vue';
+import jsonData from "./jsonData.json";
+
+export default {
+  name: 'HomeView',
+  components: {
+    GalleryCard,
+  },
+  data() {
+    return {
+      carData: jsonData,
+    };
+  },
+}
+</script>
+
+<style>
+.column {
+  display: flex;
+  flex-direction: row;
+  float: left;
+  justify-content: center
+}
+
+@media screen and (max-width: 650px) {
+  .column {
+    width: 100%;
+    display: block;
+    margin-bottom: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center
+  }
+}
+
+.dataContainer{
+ background-color: rgb(187, 234, 235);
+ border-radius: 15px;
+ margin: 25px;
+ width: 400px;
+ box-shadow: rgba(112, 112, 220, 0.1) 0px 8px 24px, rgba(223, 28, 28, 0.1) 0px 16px 56px, #f265651a 0px 24px 80px;
+}
+
+</style>
